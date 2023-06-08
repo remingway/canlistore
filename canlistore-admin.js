408327db5387125fc33b4399d7c97e96d09764ae
remingway <135838815+remingway@users.noreply.github.com>
2023-06-08 16:59:16 +0200

    Create canlistore-admin.js

---
+++ canlistore-admin.js
@@ -0,0 +1,11 @@
+// Najděte prvek, který chcete přesunout
+var elementToMove = document.querySelector('a[href="/admin/zasilkovna-plugin/"].navigation__link.navigation__link--1277');
+
+// Najděte referenční prvek, před který chcete přesunout první prvek
+var referenceElement = document.querySelector('a[href="/admin/danove-doklady/"].navigation__link.navigation__link--509');
+
+// Získejte nadřazený prvek, který obaluje oba prvky <a>
+var parentElement = referenceElement.parentNode;
+
+// Přesuňte prvek <a> před referenční prvek
+parentElement.insertBefore(elementToMove, referenceElement);
