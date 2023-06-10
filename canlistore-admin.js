72f1a89e7f594f785f9a3975971f6c266a99f452
remingway <135838815+remingway@users.noreply.github.com>
2023-06-10 15:04:09 +0200

    Update canlistore-admin.js

--- canlistore-admin.js
+++ canlistore-admin.js
@@ -1,5 +1,5 @@
 // Najděte prvek, který chcete přesunout
-console.log("20:04");
+console.log("15:04");
 var elementToMove = document.querySelector('a[href="/admin/zasilkovna-plugin/"].navigation__link.navigation__link--1277');
 
 // Najděte referenční prvek, před který chcete přesunout první prvek
