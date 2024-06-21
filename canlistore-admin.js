6ac4537aab4e9bb7485384ebf073ab39aefcee11
remingway <135838815+remingway@users.noreply.github.com>
2024-06-21 13:29:08 +0200

    Update canlistore-admin.js

--- canlistore-admin.js
+++ canlistore-admin.js
@@ -190,7 +190,7 @@ function checkOrdersOnPage() {
 				today.setHours(0, 0, 0, 0); // Set time to 00:00:00 for accurate comparison
 				const timeDiff = targetDate - today;
 				const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24)) - 1;
-				selectField1.textContent = "Ready to: " + hledanaObjednavka.datum + " (" + daysDiff + ")";
+				selectField1.textContent = "Ready&nbsp;to: " + hledanaObjednavka.datum + "&nbsp;(" + daysDiff + ")";
 				if (daysDiff == 1) {
 					selectField1.style.backgroundColor = '#99555555';
 					selectField1.style.padding =  "0px 10px 0px 10px";
