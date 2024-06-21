c90ee2ab226cb0fac168a7890f7d4a63a7f3bf4a
remingway <135838815+remingway@users.noreply.github.com>
2024-06-21 10:28:16 +0200

    Update canlistore-admin.js

--- canlistore-admin.js
+++ canlistore-admin.js
@@ -189,7 +189,7 @@ function checkOrdersOnPage() {
 				const today = new Date();
 				today.setHours(0, 0, 0, 0); // Set time to 00:00:00 for accurate comparison
 				const timeDiff = targetDate - today;
-				const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+				const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24)) - 1;
 				selectField1.textContent = "Ready to: " + hledanaObjednavka.datum + " (" + daysDiff + ")";
 				if (daysDiff == 1) {
 					selectField1.style.backgroundColor = '#99555555';
@@ -219,4 +219,4 @@ function checkOrdersOnPage() {
 }
 /* END kontrola stavu zásilek - odeslané END */
 
-console.log("verze 4.9");
+console.log("verze 5.0");
