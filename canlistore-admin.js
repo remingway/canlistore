4123364175492591876a62170c20786a1a3cbbe0
remingway <135838815+remingway@users.noreply.github.com>
2024-06-22 12:36:13 +0200

    Update canlistore-admin.js

--- canlistore-admin.js
+++ canlistore-admin.js
@@ -295,4 +295,4 @@ if (document.querySelector('tbody')) {
 
 /* END přepínání tabování mezi sloupci a řádky - další tlačítko u "uložit" END */
 
-console.log("verze 5.7");
+console.log("verze 5.8");
