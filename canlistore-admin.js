55d83a78fad09d6120d979b9eeec79e772f4adc8
remingway <135838815+remingway@users.noreply.github.com>
2025-11-20 20:43:57 +0100

    Update canlistore-admin.js

--- canlistore-admin.js
+++ canlistore-admin.js
@@ -1,6 +1,6 @@
 console.log("verze 17");
 /* procentuální sleva u akční ceny */
-if (window.location.href === "https://www.artyrium.cz/admin/ceny/") {
+if location.href.startsWith('https://www.artyrium.cz/admin/ceny/') {
     document.querySelectorAll('input[name^="actionPrice["]').forEach(actionInput => {
         const row = actionInput.closest('tr');
         if (!row) return;
